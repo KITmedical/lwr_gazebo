1f277e1e0098ad8c0d3e455286a8a5498d6d1401
ahb <andreas.bihlmaier@gmx.de>
2013-09-09 14:12:27 +0200

    rostopic pub /robots/lwr2/joints_target_safros sensor_msgs/JointState "..." works :)

--- src/LwrModelPlugin.cpp
+++ src/LwrModelPlugin.cpp
@@ -30,9 +30,23 @@ namespace gazebo
       return;
     }
 
+
     m_node = new ros::NodeHandle(m_nodeName);
     m_cartesianWriteTopicSub = m_node->subscribe<geometry_msgs::Pose>(m_cartesianWriteTopicName, 1, &LwrModelPlugin::cartesianWriteCallback, this);
     m_cartesianReadTopicPub = m_node->advertise<geometry_msgs::Pose>(m_cartesianReadTopicName, 1);
+    m_jointsWriteTopicSub = m_node->subscribe<sensor_msgs::JointState>(m_jointsWriteTopicName, 1, &LwrModelPlugin::jointsWriteCallback, this);
+    m_jointsReadTopicPub = m_node->advertise<sensor_msgs::JointState>(m_jointsReadTopicName, 1);
+
+    m_joints = m_model->GetJoints();
+    std::cout << "LWR joints:" << std::endl;
+    for (size_t jointIdx = 0; jointIdx < m_joints.size(); jointIdx++) {
+      physics::JointPtr currJoint = m_joints[jointIdx];
+      std::cout << jointIdx << " name=" << currJoint->GetName() << " angle=" << currJoint->GetAngle(0) << " v=" << currJoint->GetVelocity(0) << std::endl;
+    }
+
+    m_jointsCurrent.position.resize(m_joints.size(), 0);
+    m_jointsCurrent.velocity.resize(m_joints.size(), 0);
+    m_jointsCurrent.effort.resize(m_joints.size(), 0);
 
     // Listen to the update event. This event is broadcast every
     // simulation iteration.
@@ -43,7 +57,8 @@ namespace gazebo
   void
   LwrModelPlugin::OnUpdate()
   {
-    m_cartesianReadTopicPub.publish(m_cartesianPoseCurrent);
+    updateRobotState();
+    publishRobotState();
   }
 /*------------------------------------------------------------------------}}}-*/
 
@@ -56,6 +71,8 @@ namespace gazebo
     m_robotNamespaceName = _sdf->GetElement("robotNamespace")->Get<std::string>();
     m_cartesianReadTopicName = m_robotNamespaceName + "/" + _sdf->GetElement("cartesianReadTopic")->Get<std::string>();
     m_cartesianWriteTopicName = m_robotNamespaceName + "/" + _sdf->GetElement("cartesianWriteTopic")->Get<std::string>();
+    m_jointsReadTopicName = m_robotNamespaceName + "/" + _sdf->GetElement("jointsReadTopic")->Get<std::string>();
+    m_jointsWriteTopicName = m_robotNamespaceName + "/" + _sdf->GetElement("jointsWriteTopic")->Get<std::string>();
 
     return true;
   }
@@ -64,6 +81,36 @@ namespace gazebo
   LwrModelPlugin::cartesianWriteCallback(const geometry_msgs::Pose::ConstPtr& pose)
   {
     std::cout << "cartesianWriteCallback" << std::endl;
+  }
+
+  void
+  LwrModelPlugin::jointsWriteCallback(const sensor_msgs::JointState::ConstPtr& joints)
+  {
+    if (joints->position.size() != m_jointsCurrent.position.size()) {
+      ROS_WARN("Wrong number of joints received. Ignoring message.");
+      return;
+    }
+    for (size_t jointIdx = 0; jointIdx < m_joints.size(); jointIdx++) {
+      m_joints[jointIdx]->SetAngle(0, joints->position[jointIdx]);
+    }
+  }
+
+  void
+  LwrModelPlugin::updateRobotState()
+  {
+    for (size_t jointIdx = 0; jointIdx < m_joints.size(); jointIdx++) {
+      physics::JointPtr currJoint = m_joints[jointIdx];
+      m_jointsCurrent.position[jointIdx] = currJoint->GetAngle(0).Radian();
+      m_jointsCurrent.velocity[jointIdx] = currJoint->GetVelocity(0);
+    }
+    // TODO m_cartesianPoseCurrent
+  }
+
+  void
+  LwrModelPlugin::publishRobotState()
+  {
+    m_cartesianReadTopicPub.publish(m_cartesianPoseCurrent);
+    m_jointsReadTopicPub.publish(m_jointsCurrent);
   }
 /*------------------------------------------------------------------------}}}-*/
 
