6b600157dc77f4e508fcfd4ff670663bf7100f73
ahb <andreas.bihlmaier@gmx.de>
2013-09-09 12:04:50 +0200

    try to make everything configurable instead of having to recompile

--- include/LwrModelPlugin.h
+++ include/LwrModelPlugin.h
@@ -8,6 +8,9 @@
 #include <physics/physics.hh>
 #include <common/common.hh>
 
+#include <ros/ros.h>
+#include <geometry_msgs/Pose.h>
+
 // custom includes
 
 
@@ -41,10 +44,23 @@ namespace gazebo {
   
     private:
       // methods
+      bool loadParams(sdf::ElementPtr _sdf);
+      void cartesianWriteCallback(const geometry_msgs::Pose::ConstPtr& pose);
   
       // variables
+      // gazebo
       physics::ModelPtr m_model;
       event::ConnectionPtr m_updateConnection;
+
+      // ros
+      ros::NodeHandle* m_node;
+      ros::Subscriber m_cartesianWriteTopicSub;
+      ros::Publisher m_cartesianReadTopicPub;
+      std::string m_nodeName;
+      std::string m_robotNamespaceName;
+      std::string m_cartesianReadTopicName;
+      std::string m_cartesianWriteTopicName;
+      geometry_msgs::Pose m_cartesianPoseCurrent;
   
   
   };
--- src/LwrModelPlugin.cpp
+++ src/LwrModelPlugin.cpp
@@ -6,8 +6,6 @@
 // library includes
 #include <boost/bind.hpp>
 
-#include <ros/ros.h>
-
 // custom includes
 
 
@@ -15,9 +13,11 @@ namespace gazebo
 {
 /*---------------------------------- public: -----------------------------{{{-*/
   void
-  LwrModelPlugin::Load(physics::ModelPtr _parent, sdf::ElementPtr /*_sdf*/) 
+  LwrModelPlugin::Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf) 
   {
     std::cout << "------------------- LwrModelPlugin -------------------" << std::endl;
+
+    m_model = _parent;
 
     if (!ros::isInitialized()) {
       ROS_FATAL_STREAM("A ROS node for Gazebo has not been initialized, unable to load plugin. "
@@ -25,8 +25,14 @@ namespace gazebo
       return;
     }
 
-    // Store the pointer to the model
-    m_model = _parent;
+    if (!loadParams(_sdf)) {
+      ROS_FATAL_STREAM("Error during loadParams");
+      return;
+    }
+
+    m_node = new ros::NodeHandle(m_nodeName);
+    m_cartesianWriteTopicSub = m_node->subscribe<geometry_msgs::Pose>(m_cartesianWriteTopicName, 1, &LwrModelPlugin::cartesianWriteCallback, this);
+    m_cartesianReadTopicPub = m_node->advertise<geometry_msgs::Pose>(m_cartesianReadTopicName, 1);
 
     // Listen to the update event. This event is broadcast every
     // simulation iteration.
@@ -34,14 +40,31 @@ namespace gazebo
         boost::bind(&LwrModelPlugin::OnUpdate, this));
   }
 
-  // Called by the world update start event
   void
   LwrModelPlugin::OnUpdate()
   {
+    m_cartesianReadTopicPub.publish(m_cartesianPoseCurrent);
   }
 /*------------------------------------------------------------------------}}}-*/
 
 /*---------------------------------- private: ----------------------------{{{-*/
+  bool
+  LwrModelPlugin::loadParams(sdf::ElementPtr _sdf)
+  {
+    m_nodeName = _sdf->GetParent()->Get<std::string>("name");
+
+    m_robotNamespaceName = _sdf->GetElement("robotNamespace")->Get<std::string>();
+    m_cartesianReadTopicName = m_robotNamespaceName + "/" + _sdf->GetElement("cartesianReadTopic")->Get<std::string>();
+    m_cartesianWriteTopicName = m_robotNamespaceName + "/" + _sdf->GetElement("cartesianWriteTopic")->Get<std::string>();
+
+    return true;
+  }
+
+  void
+  LwrModelPlugin::cartesianWriteCallback(const geometry_msgs::Pose::ConstPtr& pose)
+  {
+    std::cout << "cartesianWriteCallback" << std::endl;
+  }
 /*------------------------------------------------------------------------}}}-*/
 
 
